b737f8a45d25847dc7da7c48953407e841c894d4
discorave <amandabubble@gmail.com>
2018-09-29 16:41:09 -0700

    Motified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,8 @@ from django.utils import timezone
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.TextField()
+    text = models.TextField(
+        default="test")
     created_date = models.DateTimeField(
         default=timezone.now)
     published_date = models.DateTimeField(
